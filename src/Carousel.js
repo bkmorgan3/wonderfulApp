06fbdc182654f853804559e85103c4add2432614
brad morgan <bkmorgan3@gmail.com>
2020-07-02 12:56:30 -0700

    implements carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
-const Carousel = () => <div>I am photos</div>;
+import imageOne from './assets/ls-f-sport.jpg';
+import imageTwo from './assets/ls-hybrid.jpg';
+import imageThree from './assets/ls.jpg';
 
-export default Carousel;
+const MyCarousel = () => (
+  <Carousel centered infinite arrows slidesPerPage={2}>
+    <img src={imageOne} alt="black car" />
+    <img src={imageTwo} alt="red car" />
+    <img src={imageThree} alt="white car" />
+  </Carousel>
+);
+
+export default MyCarousel;
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Carousel from './Carousel';
+import MyCarousel from './Carousel';
+import Map from './Map';
 
 const Main = () => (
   <div>
     <h1>I am the main holder</h1>
-    <Carousel />
+    <MyCarousel />
+    <Map />
   </div>
 );
 
