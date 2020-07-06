016737f4aeaa6c8fcbbb46c0d5ff8bd1966096bd
brad morgan <bkmorgan3@gmail.com>
2020-07-05 18:41:54 -0700

    adds data file and Markers

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from 'react-google-maps';
+
+import data from './data/data.json';
+console.log('data', data);
 
 function Map() {
   return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 40.7831, lng: -73.9712 }}
-    />
+    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 40.7831, lng: -73.9712 }}>
+      {data.dealers.map((d) => (
+        <Marker
+          key={d.id}
+          position={{
+            lat: d.position.lat,
+            lng: d.position.lng,
+          }}
+        />
+      ))}
+    </GoogleMap>
   );
 }
 
