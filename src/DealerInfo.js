2b4bcf51e3beff4dede5925aadf7de6568dc8701
brad morgan <bkmorgan3@gmail.com>
2020-07-07 16:20:41 -0700

    rendering for 0 cars

--- src/DealerInfo.js
+++ src/DealerInfo.js
@@ -1,36 +1,48 @@
 import React from 'react';
 
 const DealerInfo = (props) => {
-  console.log('props', props);
   return (
     <div>
-      <h4>Availability for {props.location}</h4>
-      <div>
-        {props.trims.map((t) => (
-          <div
-            style={{
-              // border: '2px solid green',
-              margin: '10px',
-              display: 'flex',
-            }}
-          >
-            <div className="car-detail">
-              <img
-                style={{ width: '100px', height: '100px' }}
-                src={t.image}
-                alt="car pic"
-              />
-            </div>
-            <div className="details">
-              <h3>2020 {t.name.toUpperCase()}</h3>
-              <p>Starting at {t.price}</p>
-              <button className="request-btn">Request Quote</button>
-            </div>
+      {props.trims.length ? (
+        <div>
+          <h4>Availability for {props.location}</h4>
+          <div>
+            {props.trims.map((t) => (
+              <div
+                style={{
+                  margin: '10px',
+                  display: 'flex',
+                }}
+              >
+                <div className="car-detail">
+                  <img
+                    style={{ width: '100px', height: '100px' }}
+                    src={t.image}
+                    alt="car pic"
+                  />
+                </div>
+                <div className="details">
+                  <h3>2020 {t.name.toUpperCase()}</h3>
+                  <p>Starting at {t.price}</p>
+                  <button className="request-btn">Request Quote</button>
+                </div>
+              </div>
+            ))}{' '}
           </div>
-        ))}
-      </div>
+        </div>
+      ) : (
+        <div>NO</div>
+      )}
     </div>
   );
 };
 
 export default DealerInfo;
+
+//         ) : (
+//         <div>
+//           <h4>There are no options for {props.location}</h4>
+//         </div>
+//       )
+//   }
+// </div>
